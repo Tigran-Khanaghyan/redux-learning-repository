3a32fd3b946baed6ce92a16c5df89cd1bb088891
Tigran Khanaghyan <khanaghyan.tigran@gmail.com>
2021-10-11 17:27:04 +0400

    New Sign in project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment, setStep } from "./actions";
+
 
 function App() {
-  const counter = useSelector((state) => state.counter);
-  const step = useSelector((state) => state.step);
-  const [value, setValue] = useState(0);
-
-  const dispatch = useDispatch();
-
   return (
-    <div>
-      <div className="App">Counter: {counter}</div>
-      <button onClick={() => dispatch(increment(Number(step)))}>+</button>
-      <button onClick={() => dispatch(decrement(Number(step)))}>-</button>
-      <input type="number" onChange={(e) => setValue(e.target.value)}></input>
-      <button onClick={() => dispatch(setStep(value))}>Set Step</button>
-    </div>
-  );
+    <div>Hello</div>
+  )
 }
 
 export default App;
